da87d7c6b75cc319faa56c05d7d8e66ae4f98467
Lea Larrama <leandrolarrama@hotmail.com>
2023-04-01 15:06:19 +0100

    arreglos

---
+++ src/components/Work.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import WorkItem from "./WorkItem"
+
+const Work = () => {
+  return (
+    <div>
+      <h1 className='sm:text-5xl text-4xl font bold text-gray-800'>Work</h1>
+        <WorkItem/>
+    </div>
+  )
+}
+
+export default Work
---
+++ src/components/WorkItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const WorkItem = () => {
+  return (
+    <div>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus libero aut eaque, ab esse enim animi accusamus illum beatae, reprehenderit ratione nesciunt omnis repellat reiciendis unde ex qui at quisquam.</p>
+    </div>
+  )
+}
+
+export default WorkItem
