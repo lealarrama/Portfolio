4151f91ecafdb18e3cc6c1f0c257d2602af1066a
Lea Larrama <leandrolarrama@hotmail.com>
2024-02-18 18:11:08 +0000

    arrangements

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Logo from '../assets/img/logo.png'
 
 const Footer = () => {
   return (
-    <div className='pb-5'>
-      <h2 className ='text-center text-xl text-stone-500'>By Leandro Larrama Klam</h2>
+    <div className='pb-5 flex justify-center'>
+    <div className='flex items-center'>
+      <img className='rounded-full w-16 h-16 mr-2' src={Logo} alt='' />
+      <h2 className='text-xl text-stone-500'>By Leandro Larrama Klam</h2>
     </div>
+  </div>
   )
 }
 
